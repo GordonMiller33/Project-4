f066669d8fbdc1dc9fe9f9fb17cc8d506b30fb58
Matt S. <ms3099@rutgers.edu>
2021-04-06 23:01:23 -0400

    subtotal for coffee

--- src/cafe/Main.java
+++ src/cafe/Main.java
@@ -6,7 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
+/**
+ * 
+ * @author Matthew Schilling and Gordon Miller
+ * The main class of the cafe launches the main menu controller
+ *
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
