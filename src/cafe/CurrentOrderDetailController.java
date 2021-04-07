a7d4f1f4523ca86bd399c2e8bdd2c3d533e0983d
Gordon <gordonmiller33@gmail.com>
2021-04-06 22:18:22 -0400

    Wait it works?

---
+++ src/cafe/CurrentOrderDetailController.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package cafe;
+
+import java.io.IOException;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+/**
+ * @author Me
+ *
+ */
+public class CurrentOrderDetailController {
+	private final MainMenuController mainMenu;
+	
+	private Stage curOrderDetailStage;
+	
+	@FXML
+	private ObservableList<Order> orderList;
+	@FXML
+	private Button removeBtn, placeOrderBtn;
+	@FXML
+	private ListView curOrderListView;
+	
+	public CurrentOrderDetailController(MainMenuController mainMenu) {
+		this.mainMenu = mainMenu;
+		curOrderDetailStage = new Stage();
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("CurrentOrderDetail.fxml"));
+			loader.setController(this);
+			curOrderDetailStage.setScene(new Scene(loader.load()));
+			for(int i=0;i<mainMenu.curOrder.size();i++) {
+				curOrderListView.getItems().add(mainMenu.curOrder.get(i).toString());
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	@FXML
+	private void initialize() {
+		removeBtn.setOnAction(event -> removeItem());
+	}
+	
+	/**
+	 * The setStage method will display the order detail window
+	 */
+	public void setStage() {
+		curOrderDetailStage.showAndWait();
+	}
+	private void removeItem() {
+		int index = curOrderListView.getSelectionModel().getSelectedIndex();
+		mainMenu.curOrder.remove(index);
+		curOrderListView.getItems().remove(index);
+	}
+}
---
+++ src/cafe/MainMenuController.java
@@ -0,0 +1,124 @@
+package cafe;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+public class MainMenuController {
+	
+	@FXML
+    private Button donutMenu, coffeeMenu, currentOrderBtn, storeOrdersBtn;
+	
+	@FXML
+	private ListView<String> userOrderView, allOrdersView;
+	
+	@FXML
+	private ObservableList<MenuItem> currentOrderList, storeOrdersList;
+	
+	private ArrayList<Order> storeOrders = new ArrayList<Order>();
+	protected ArrayList<MenuItem> curOrder = new ArrayList<MenuItem>();
+	private int orderNum = 1;
+	
+	private final Stage mainStage;
+	
+	
+	/**
+	 * Using the FXML loader will let us have references between the main menu GUI the other GUIs
+	 */
+	public MainMenuController() {
+		mainStage = new Stage();
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+			loader.setController(this);
+			mainStage.setScene(new Scene(loader.load()));
+			mainStage.setTitle("Welcome to RU Cafe!");
+			
+		}catch (IOException e) {
+            e.printStackTrace();
+	}
+}
+
+	/**
+	 * The set stage method will initially display the main menu
+	 */
+	public void showStage() {
+		mainStage.showAndWait();
+		
+	}
+	/**
+	 * This method creates the necessary button associations on load
+	 */
+	@FXML
+	private void initialize() {
+		donutMenu.setOnAction(event -> showDonutMenu());
+		coffeeMenu.setOnAction(event -> showCoffeeMenu());
+		currentOrderBtn.setOnAction(event -> showCurOrderDetails());
+		storeOrdersBtn.setOnAction(event -> showStoreOrderDetails());
+	}
+	
+	/**
+	 * This method will allow the donut ordering screen to load
+	 */
+	private void showDonutMenu() {
+		OrderingDonutsController donutController = new OrderingDonutsController(this);
+		donutController.setStage();
+	}
+	/**
+	 * This method will allow the coffee ordering screen to load
+	 */
+	private void showCoffeeMenu() {
+		OrderingCoffeeController coffeeController = new OrderingCoffeeController(this);
+		coffeeController.setStage();
+	}
+	/**
+	 * This method will allow the current order details screen to load
+	 */
+	private void showCurOrderDetails() {
+		CurrentOrderDetailController curOrderController = new CurrentOrderDetailController(this);
+		curOrderController.setStage();
+	}
+	/**
+	 * This method will allow the store order details screen to load
+	 */
+	private void showStoreOrderDetails() {
+		StoreOrdersController storeOrderController = new StoreOrdersController(this);
+		storeOrderController.setStage();
+	}
+	
+	/**
+	 * The addOrder method adds a donut or coffee order to the current list of orders
+	 */
+	public void addOrder() {
+		
+	}
+	
+	/**
+	 * The removeOrder method removes a selected order from the current list of orders
+	 */
+	public void removeOrder() {
+		
+	}
+	
+	/**
+	 * The finalizeOrders method adds the current list of orders as a new object to the list
+	 * of all cafe orders.
+	 */
+	private void finalizeOrders() {
+		
+	}
+	/**
+	 * THe add menu item method adds a coffee or donut(s) to the list of menu items
+	 * this list will become an order once the user finalizes
+	 * @param item the donut or coffee to be added.
+	 */
+	public void addMenuItem(MenuItem item) {
+		curOrder.add(item);
+	}
+	}
